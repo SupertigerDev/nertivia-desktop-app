7e323a6d91847660444467c9c306cc1af09b7690
supertiger1234 <saawalkhn123@googlemail.com>
2019-08-11 21:25:26 +0100

    minimise to tray

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain, } = require("electron");
+const { app, BrowserWindow, ipcMain, Tray, Menu} = require("electron");
 const log = require('electron-log');
 const isDev = require('electron-is-dev');
 const { autoUpdater } = require("electron-updater");
@@ -36,13 +36,11 @@ const readyEvent = _ => {
   })
 
   autoUpdater.on('error', (err) => {
+    if (isDev)
+      loadMainWindow()
+      updaterWindow.close();
     autoUpdater.removeAllListeners()
     sendUpdaterMessages('ERROR', err.message)
-    if (isDev)
-      setTimeout(() => {
-        //loadMainWindow()
-        //updaterWindow.close();
-      }, 1000);
   })
 
   autoUpdater.on('update-available', (progressObj) => {
@@ -75,16 +73,54 @@ app.on("active", _ => {
 function loadMainWindow() {
   if (mainWindow !== null) return;
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     minHeight: 300,
     minWidth: 350,
     frame: false
   });
   mainWindow.loadURL("https://nertivia.tk/login");
+  // mainWindow.loadURL("http://localhost:8080/login");
 
-  mainWindow.on("close", _ => {
-    mainWindow = null;
-    app.quit();
+  mainWindow.on("close", event => {
+    if(!app.isQuiting){
+      event.preventDefault();
+      mainWindow.hide();
+    }
+    return false;
   });
+
+  var contextMenu = Menu.buildFromTemplate([
+    { label: 'Show App', click:  function(){
+        mainWindow.show();
+    } },
+    { label: 'Quit', click:  function(){
+        app.isQuiting = true;
+        app.quit();
+    } }
+  ]);
+
+
+  var appIcon = null;
+  appIcon = new Tray('./build/icon.ico');
+
+  var contextMenu = Menu.buildFromTemplate([
+    { label: 'Show App', click:  function(){
+        mainWindow.show();
+    } },
+    { label: 'Quit', click:  function(){
+        app.isQuiting = true;
+        app.quit();
+    } }
+  ]);
+
+  appIcon.setToolTip('Nertivia');
+  
+  appIcon.on('click', () => {
+    mainWindow.show();
+  })
+  appIcon.setContextMenu(contextMenu);
+
+
+
 }
