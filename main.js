d20d2d5ff2e1ab73da99f1b33ba3455f4bfd54e5
Supertiger <saawalkhn@googlemail.com>
2021-03-17 11:51:40 +0000

    single instance lock

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const {app, BrowserWindow} = require("electron");
-const {loadAppWindow} = require("./appWindow");
+const {loadAppWindow, getAppWindow} = require("./appWindow");
 const updaterWindow = require("./updaterWindow")
 const getTray = require("./utils/TrayInstance");
 const DEV_MODE = true;
@@ -7,8 +7,24 @@ let URL = "https://beta.nertivia.net/app";
 if (DEV_MODE) {
   URL = "http://localhost:8080/app";
 }
+const singleInstanceLock = app.requestSingleInstanceLock()
+
+app.on('second-instance', (event, argv, cwd) => {
+  if (getAppWindow()) {
+    getAppWindow().show();
+    if (getAppWindow().isMinimized()) getAppWindow().restore();
+    getAppWindow().focus();
+  }
+})
+
+
+
 
 function ready() {
+  if (!singleInstanceLock){
+    app.quit();
+    return;
+  }
   getTray();
   updaterWindow(DEV_MODE, done => {
     if (!done) return;
